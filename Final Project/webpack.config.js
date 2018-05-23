68a41e3c1bb61cbdca8231358f6ac10bc3cf7416
mcdunn51 <mattdunningweb@outlook.com>
2018-05-23 14:45:49 +0100

    configuring babel

--- Final Project/webpack.config.js
+++ Final Project/webpack.config.js
@@ -1,12 +1,32 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/js/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist/js'),
-		filename:'bundle.js'
+		path: path.resolve(__dirname, 'dist'),
+		filename:'js/bundle.js'
 	},
 	devServer: {
 		contentBase: './dist'
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		}) 
+	],
+	//loaders
+	module: {
+		rules: [
+			{
+				//a regular expression telling all javascript files to use the babel loader
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
 	}
 };
